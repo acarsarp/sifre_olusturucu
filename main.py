e9b3e60a67e79e9bf527783c48c7bd528c1b0bbf
acarsarp <163336919+acarsarp@users.noreply.github.com>
2024-03-20 21:04:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+karakterler  ="+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+
+def sifre_olustur(uzunluk):
+    sifre = ""
+    for i in range(sifre_uzunlugu):
+        sifre += random.choice(karakterler)
+    print(sifre)
+    return sifre
+while True:
+    hesap = input("Hangi hesabın için şifre istiyorsun?\n")
+    sifre_uzunlugu = int(input("Kaç karakterli olsun  şifre eyyy 4.MURAT GURZUU?\n"))
+    if sifre_uzunlugu <= 0:
+        break
+    with open("sifreler.txt", "a") as doc:
+        doc.write(f"{hesap}: {sifre_olustur(sifre_uzunlugu)}")
